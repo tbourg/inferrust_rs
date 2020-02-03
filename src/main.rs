6729ca7686f608db62dd94922fd14f66f8d1b456
Thomas Bourg <tbourg07@gmail.com>
2020-02-03 17:21:49 +0100

    update: subset of a graph

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 use sophia::graph::{inmem::LightGraph, *};
 use sophia::parser;
 use sophia::serializer;
 use sophia::serializer::TripleStringifier;
+use sophia::term::Term;
 use sophia::triple::stream::{TripleSink, TripleSource};
 
+fn help() {
+    println!("Usage");
+    process::exit(0);
+}
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        3 => (),
+        _ => help(),
+    }
+    let operator = &args[1];
+    let operand = &args[2];
+    match &operator[..] {
+        "o" | "s" | "p" => (),
+        _ => help(),
+    }
     let filename = "res/sample.ttl";
     let content = fs::read_to_string(filename).expect("Error reading file.");
     let mut graph = LightGraph::new();
@@ -14,7 +31,15 @@ fn main() {
         .in_graph(&mut graph)
         .expect("Error loading graph.");
 
-    let mut nt_stringifier = serializer::nt::stringifier();
-    let example = nt_stringifier.stringify_graph(&mut graph).unwrap();
-    println!("The resulting graph\n{}", example);
+    operand.foo();
+
+    /*    let operand_term = Term::new_iri(operand).unwrap();
+        let results = match &operator[..] {
+            "s" => graph.triples_with_s(&operand_term)
+        };
+
+        let mut nt_stringifier = serializer::nt::stringifier();
+        let example = nt_stringifier.stringify_graph(&mut graph).unwrap();
+        println!("The resulting graph\n{}", example);
+    */
 }
