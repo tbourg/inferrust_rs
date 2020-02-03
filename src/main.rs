e4f6d23de9667a59436a335309379198eb96c263
Thomas Bourg <tbourg07@gmail.com>
2020-02-03 16:42:10 +0100

    display a graph stored in a file

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,20 @@
-use sophia::graph::{*, inmem::LightGraph};
-use sophia::ns::Namespace;
+use std::{env, fs};
+
+use sophia::graph::{inmem::LightGraph, *};
 use sophia::parser;
 use sophia::serializer;
 use sophia::serializer::TripleStringifier;
 use sophia::triple::stream::{TripleSink, TripleSource};
 
-fn main () {
-    let example = r#"
-        @prefix : <http://example.org/>.
-        @prefix foaf: <http://xmlns.com/foaf/0.1/>.
-
-        :alice foaf:name "Alice";
-            foaf:mbox <mailto:alice@work.example> .
-
-        :bob foaf:name "Bob".
-    "#;
+fn main() {
+    let filename = "res/sample.ttl";
+    let content = fs::read_to_string(filename).expect("Error reading file.");
     let mut graph = LightGraph::new();
-    parser::turtle::parse_str(example).in_graph(&mut graph);
-
-    let ex = Namespace::new("http://example.org/").unwrap();
-    let foaf = Namespace::new("http://xmlns.com/foaf/0.1/").unwrap();
-    let bob = ex.get("bob").unwrap();
-    let knows = foaf.get("known").unwrap();
-    let alice = ex.get("alice").unwrap();
-    graph.insert(&bob, &knows, &alice).unwrap();
+    parser::turtle::parse_str(&content)
+        .in_graph(&mut graph)
+        .expect("Error loading graph.");
 
     let mut nt_stringifier = serializer::nt::stringifier();
-    let example2 = nt_stringifier.stringify_graph(&mut graph).unwrap();
-    println!("The resulting graph\n{}", example2);
+    let example = nt_stringifier.stringify_graph(&mut graph).unwrap();
+    println!("The resulting graph\n{}", example);
 }
