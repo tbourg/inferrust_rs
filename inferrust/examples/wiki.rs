2de84f87b5f1468c937d451f413e71fc513ac552
Thomas Bourg <tbourg07@gmail.com>
2020-04-15 18:00:05 +0200

    Re-bench

--- inferrust/examples/wiki.rs
+++ inferrust/examples/wiki.rs
@@ -16,14 +16,23 @@ fn main() {
         RuleProfile::RhoDF(),
         RuleProfile::RDFSPlus(),
     ];
-    fs::read_dir("inferrust/res/bsbm")
+    fs::read_dir("inferrust/res/wiki")
         .unwrap()
         .for_each(|file| {
             println!("file: {:#?}", &file);
             let rep = fs::read_to_string(file.unwrap().path()).unwrap();
             profiles.iter_mut().for_each(|profile| {
+                let t0 = precise_time_ns();
                 let ts = sophia::parser::turtle::parse_str(&rep);
+                let t1 = precise_time_ns();
+                let time = (t1 - t0) as f64 / 1e9;
+                println!("parsing: {}", time);
+                let t0 = precise_time_ns();
                 let mut i_graph = InfGraph::from(ts);
+                let t1 = precise_time_ns();
+                let time = (t1 - t0) as f64 / 1e9;
+                println!("creation: {}", time);
+                std::process::exit(127);
                 println!("graph size: {}", i_graph.size());
                 let t0 = precise_time_ns();
                 i_graph.process(profile);
