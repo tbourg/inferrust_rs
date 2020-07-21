81d812c825dc2897c0821c621e413170724375f6
Thomas Bourg <tbourg07@gmail.com>
2020-07-21 09:44:12 +0200

    fix bench

--- inferrust/benches/inferrust.rs
+++ inferrust/benches/inferrust.rs
@@ -6,9 +6,7 @@ use inferrust::rules::*;
 pub fn simpsons_total(c: &mut Criterion) {
     c.bench_function("simpsons_total", |b| {
         b.iter(|| {
-            let mut graph = InfGraph::from(sophia::parser::turtle::parse_bufread(
-                std::io::BufReader::new(std::fs::File::open("benches/res/simpsons.nt").unwrap()),
-            ));
+            let mut graph = InfGraph::from(sophia::parser::turtle::parse_str(SIMPSONS));
             assert_eq!(graph.size(), 11);
             graph.process(&mut RuleProfile::RDFSPlus());
             // assert_eq!(graph.size(), 57);
@@ -16,5 +14,31 @@ pub fn simpsons_total(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, simpsons_total);
+pub fn simpsons_load(c: &mut Criterion) {
+    c.bench_function("simpsons_load", |b| {
+        b.iter(|| {
+            let mut graph = InfGraph::from(sophia::parser::turtle::parse_str(SIMPSONS));
+            assert_eq!(graph.size(), 11);
+        })
+    });
+}
+
+criterion_group!(benches, simpsons_total, simpsons_load);
 criterion_main!(benches);
+
+const SIMPSONS: &str = r#"@prefix : <http://example.org/> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix owl: <http://www.w3.org/2002/07/owl#> .
+
+:Bart rdf:type :human .
+:Lisa rdf:type :human .
+:BLOB a :entity .
+:human rdfs:subClassOf :mammal .
+:mammal rdfs:subClassOf :animal .
+:animal rdfs:subClassOf :entity .
+:entity rdfs:subClassOf :animal .
+:Bart :enfant :Lisa .
+:enfant owl:inverseOf :parent .
+:parent owl:equivalentProperty :geniteur . 
+:progeniture owl:equivalentProperty :enfant ."#;
