c620e2bf8d0d4587c8e12a6bb42b11f4321228b3
Thomas Bourg <tbourg07@gmail.com>
2020-07-30 09:30:06 +0200

    new rule on list

--- inferrust/src/rules/list_rules.rs
+++ inferrust/src/rules/list_rules.rs
@@ -141,6 +141,74 @@ fn process_cls_false(ts: &TripleStore, member_idx: usize) -> RuleResult {
     output
 }
 
+pub fn CAX_ADC(ts: &TripleStore) -> RuleResult {
+    let mut output = vec![];
+    let rdf_type = ts.elem().get(NodeDictionary::prop_idx_to_idx(
+        NodeDictionary::rdftype as u64,
+    ));
+    if rdf_type == None {
+        return output;
+    }
+    let rdf_type_rev = rdf_type.unwrap().os();
+    let rdf_type = rdf_type.unwrap().so();
+    let diff = NodeDictionary::owlallDisjointClasses as u64;
+    if rdf_type_rev[0][0] > diff || rdf_type_rev[rdf_type_rev.len() - 1][0] < diff {
+        return output;
+    }
+    let pairs_members = ts.elem().get(NodeDictionary::prop_idx_to_idx(
+        NodeDictionary::members as u64,
+    ));
+    if pairs_members == None {
+        return output;
+    }
+    let pairs_members = pairs_members.unwrap().so();
+    for [o_t, s_t] in rdf_type_rev {
+        if *o_t > diff {
+            break;
+        } else if *o_t == diff {
+            for [s_m, o_m] in pairs_members {
+                if *s_m > *s_t {
+                    break;
+                } else if *s_m == *s_t {
+                    let members = &ts.list(*o_m).unwrap().elems;
+                    let len = members.len();
+                    for i in 0..len {
+                        for [o, s] in rdf_type_rev {
+                            let member_i = members[i];
+                            if *o > member_i {
+                                break;
+                            } else if *o == member_i {
+                                for j in i + 1..len {
+                                    let member_j = members[j];
+                                    if binary_search_pair(rdf_type, [*s, member_j]) {
+                                        output.push(INVALID);
+                                        return output;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    output
+}
+
+pub fn EQ_DIFF2(ts: &TripleStore) -> RuleResult {
+    process_cls_false(
+        ts,
+        NodeDictionary::prop_idx_to_idx(NodeDictionary::members as u64),
+    )
+}
+
+pub fn EQ_DIFF3(ts: &TripleStore) -> RuleResult {
+    process_cls_false(
+        ts,
+        NodeDictionary::prop_idx_to_idx(NodeDictionary::owldistinctmembers as u64),
+    )
+}
+
 pub fn PRP_SPO2(ts: &TripleStore) -> RuleResult {
     fn intern(ts: &TripleStore, step: u64, chain: &[u64]) -> Vec<u64> {
         if chain.is_empty() {
@@ -153,8 +221,9 @@ pub fn PRP_SPO2(ts: &TripleStore) -> RuleResult {
         let pairs = pairs.unwrap().so();
         pairs
             .iter()
-            .filter(|[s, o]| *s == step)
-            .flat_map(|[s, o]| intern(ts, *o, &chain[1..]))
+            .take_while(|[s, _]| *s <= step)
+            .filter(|[s, _]| *s == step)
+            .flat_map(|[_, o]| intern(ts, *o, &chain[1..]))
             .collect()
     };
     let mut output = vec![];
@@ -180,5 +249,11 @@ pub fn PRP_SPO2(ts: &TripleStore) -> RuleResult {
             }
         }
     }
+    output
+}
+
+pub fn PRP_KEY(ts: &TripleStore) -> RuleResult {
+    let mut output = vec![];
+    // TO DO
     output
 }
